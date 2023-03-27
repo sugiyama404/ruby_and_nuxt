e5313be9b4665f4ff538c7e0600f5ff091ae7e64
sugiyama404 <mmingriha@gmail.com>
2023-03-28 08:06:43 +0900

    add migration conf for github actions

--- apserver/app/spec/models/todo_spec.rb
+++ apserver/app/spec/models/todo_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "create recode" do
+    todo = FactoryBot.create(:todo)
+    expect(todo).to be_valid
+  end
 end
--- apserver/app/spec/requests/api/todos_spec.rb
+++ apserver/app/spec/requests/api/todos_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Todos", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /api/todos" do
+    it "check creat method" do
+      valid_params = { content: "test_api" }
+      expect { post "/api/todos", params: valid_params.to_json, headers: { "Content-Type" => "application/json" }}.to change(Todo, :count).by(+1)
+      expect(response.status).to eq(201)
+    end
   end
+  describe "GET /api/todos" do
+    it "check index method" do
+      get '/api/todos'
+      expect(response.status).to eq(200)
+    end
+  end
+  #describe "GET /api/todos?id=x" do
+  #  it "check show method" do
+  #  end
+  #end
+  #describe "PUT /api/todos/x" do
+  #  it "check update method" do
+  #    todo = FactoryBot.create(:todo)
+  #  end
+  #end
+  #describe "DELETE /api/todos/x" do
+  #  it "check delete method" do
+  #  end
+  #end
 end
