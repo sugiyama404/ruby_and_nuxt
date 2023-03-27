ef166607695ac2bfcc8c9ea00e20e8a84b15c1d7
sugiyama404 <mmingriha@gmail.com>
2023-03-27 12:55:32 +0900

    up

--- apserver/app/config/routes.rb
+++ apserver/app/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "index#index"
   namespace 'api' do
-    resources :todos, only:[:index] do
+    resources :todos, only:[:index, :show, :create, :destroy, :update] do
     end
   end
 end
