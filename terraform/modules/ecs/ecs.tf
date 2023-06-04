a323a351872772364a9cbfd72277546047ed75bc
sugiyama404 <mmingriha@gmail.com>
2023-06-04 22:30:54 +0900

    delete comment out

--- terraform/modules/ecs/ecs.tf
+++ terraform/modules/ecs/ecs.tf
@@ -15,9 +15,4 @@ resource "aws_ecs_cluster_capacity_providers" "capacity_provider" {
   cluster_name       = "${var.app_name}-cluster"
   capacity_providers = ["FARGATE"]
 
-  # default_capacity_provider_strategy {
-  #   base              = 1
-  #   weight            = 100
-  #   capacity_provider = "FARGATE"
-  # }
 }
